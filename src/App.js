b3369a2a9f9840391889bbff0ee560da2a51ae19
SagaevRamzan <80630411+SagaevRamzan@users.noreply.github.com>
2021-05-27 23:20:32 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import {useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [first_par,setFirst_par]=useState("")
+    const [second_par,setSecond_par]=useState("")
+
+
+    function handleFirstPar(e){
+
+        setFirst_par(e.target.value)
+
+
+
+    }
+    function handleSecondtPar(e){
+        setSecond_par(e.target.value)
+
+    }
+
+
+    function AddChangePas(){
+        if(first_par==0 || second_par==0){
+                     alert("Поле пароля пустое, введите пароль")
+        }
+            else {
+                    if(first_par!==second_par){
+                           alert("Пароль не верен")
+                    }
+                    else {
+                        if(first_par===second_par){
+                            alert("Добро пожаловать, вы вошли в свой аккаунт")
+                        }
+                    }
+             }
+
+
+    }
+
+
+
+
+
+    return (<div className="app" >Введите от 1 до 8 символов
+              <div className="string_1">
+                <div className="text">Password first</div>
+                <input maxLength={8} className="par_first" onChange={handleFirstPar}></input>
+              </div>
+              <div className="string_2">
+                <div className="text">Password repet</div>
+                 <input  maxLength={8} className="par_sec" onChange={handleSecondtPar}></input>
+              </div>
+              <button className="btn" onClick={AddChangePas}>Добавить</button>
+         </div>)
+
 }
 
 export default App;
